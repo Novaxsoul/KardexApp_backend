a503b18ff8fd4fe2da726e48b5ea8204e2fb4942
Juan Cortés <jecortes27@misena.edu.com>
2020-03-16 13:00:42 -0500

    Clase para configurar la api

---
+++ src/kardexapi/services/RestApplication.java
@@ -0,0 +1,11 @@
+package kardexapi.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+// Clase para definir la url base de la API
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
